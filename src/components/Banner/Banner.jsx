9a9d4b8897ec3163b66feeeb74079e487d91e87e
Misbah Hasan <tahermisbah41@gmail.com>
2021-03-11 21:59:00 +0600

    README.md file updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,11 +2,14 @@
 import React from 'react';
 import './Banner.css';
 import homeBanner from '../Home/img-banner/banner.jpg'
+import bannerLogo from './logo.png';
 const Banner = () => {
   return (
     <>
       <div style={{ backgroundImage: `linear-gradient(0deg, rgb(126 126 126 / 56%), rgb(58 58 58 / 79%)),url(${homeBanner})` }} className=" banner">
-        <h2>The English Premier Sport Leagues</h2>
+        <div className='bannerImg'>
+          <img src={bannerLogo} alt=""/>
+        </div>
       </div>
     </>
   );
--- src/components/League/League.jsx
+++ src/components/League/League.jsx
@@ -9,7 +9,7 @@ const League = (props) => {
   const {idLeague, strLeague, strSport } = props.league;
   
   return (
-    <div className=' p-2 pb-4 mt-5 league-card'>
+    <div className='  pb-4 mt-5 league-card'>
       <h4 className='sportTitle'>{strLeague}</h4>
       <p>Sport Type : {strSport}</p>
       <Link to={'/leagueDetail/'+idLeague}><button className='btn-detail'><span>More Detail</span></button></Link>
